c11693fa3bd5349bd991a2f2923502fb8930e1ea
emilio <e.meroni1@studenti.unibg.it>
2025-11-19 16:25:28 +0100

    refactoring

--- 18-calcola-distanza-centro/main.cpp
+++ 18-calcola-distanza-centro/main.cpp
@@ -1,86 +1,38 @@
 #include <iostream>
 #include "opencv2/opencv.hpp"
 
-using namespace std;
-using namespace cv;
-
-/**
- * restituisce il punto centrale dell'immagine
- */
-Point2i get_center_image(Mat img);
-
-/**
- * disegna un punto sull'immagine, e sopra ci scrive un testo
- */
-void scrivi_testo(Mat &frame, Point2i &punto, String &testo);
-
-/**
- * restituisce la distanza su x nel punto centrale inquadrato dalla telecamera
- */
-Point2f calcola_distanza_centro(float altezza, float angolo_deg);
-
-/**
- * crea una trackbar
- */
-void create_trackbar();
-
-Scalar colore_centro(0, 255, 0);
-char nome_window_immagine[50] = "Immagine";
-char nome_window_trackbar[50] = "Trackbar";
-
-int altezza_int = 90;      //[cm]
-int angolo_deg_int = 6000; //[cdeg]
-
-int main()
-{
-    VideoCapture video_capture(0);
-    Mat frame;
-    namedWindow(nome_window_immagine);
-    create_trackbar();
-    char testo[100];
-
-    while (true)
-    {
-        video_capture >> frame;
-        Point2i center_image = get_center_image(frame);
-        Point2f distanza = calcola_distanza_centro(((float)altezza_int) / 100, ((float)angolo_deg_int) / 100);
-        sprintf(testo, "distanza: %.2f m", distanza.x);
-        scrivi_testo(frame, center_image, testo);
-        imshow(nome_window_immagine, frame);
-        waitKey(10);
-    }
-
-    video_capture.release();
-}
+char nome_window_immagine[100] = "Trackbar";
+int altezza_int = 100;
+int angolo_deg_int = 10;
 
 cv::Point2f calcola_distanza_centro(float altezza, float angolo_deg)
 {
     float angolo = M_PI * angolo_deg / 180.0;
     float distanza = tan(angolo) * altezza;
-    return Point2f(distanza, 0);
+    return cv::Point2f(distanza, 0);
 }
 
-Point2i get_center_image(Mat img)
+cv::Point2i get_center_image(cv::Mat img)
 {
-    Point2i center(img.size().width / 2, img.size().height / 2);
+    cv::Point2i center(img.size().width / 2, img.size().height / 2);
     return center;
 }
 
 void create_trackbar()
 {
     // namedWindow(nome_window_trackbar, WINDOW_AUTOSIZE);
-    createTrackbar("Altezza", nome_window_immagine, &altezza_int, 200);
-    createTrackbar("Angolo", nome_window_immagine, &angolo_deg_int, 9000);
+    cv::createTrackbar("Altezza", nome_window_immagine, &altezza_int, 200);
+    cv::createTrackbar("Angolo", nome_window_immagine, &angolo_deg_int, 9000);
 }
 
 /**
  * Calcola la posizione (relativa alla base della telecamera) di un punto su una immagine.
  *
- * @param[in] lunghezza_focale      lunghezza focale della telecamera in pixel.
- * @param[in] dimensione_immagine   dimensioni dell'immagine in pixel (altezza e larghezza).
- * @param[in] punto_immagine        posizione del punto (in pixel) nell'immagine da calcolare la distanza.
- * @param[in] angolo                angolo di beccheggio della telecamera in gradi.
- * @param[in] altezza               altezza della telcamera in metri.
+ * @param lunghezza_focale      lunghezza focale della telecamera in pixel.
+ * @param dimensione_immagine   dimensioni dell'immagine in pixel (altezza e larghezza).
+ * @param punto_immagine        posizione del punto (in pixel) nell'immagine da calcolare la distanza.
+ * @param angolo                angolo di beccheggio della telecamera in gradi.
+ * @param altezza               altezza della telcamera in metri.
  *
  * @return cv::Point2f posizione x e y del punto.
  */
@@ -104,13 +56,13 @@ cv::Point2f calcola_distanza(float lunghezza_focale, cv::Size dimensione_immagin
     double world_x = altezza * tan(angolo_totale_verticale);
     double world_y = world_x * tan(theta_orizzontale);
 
-    return Point2f(world_x, world_y);
+    return cv::Point2f(world_x, world_y);
 }
 /**
  * Stima della lunghezza focale in pixel.
  *
- * @param[in] larghezza_immagine larghezza dell'immagine in pixel
- * @param[in] fov_orizzontale fov orizzontale in gradi
+ * @param larghezza_immagine larghezza dell'immagine in pixel
+ * @param fov_orizzontale fov orizzontale in gradi
  *
  * @return lunghezza focale in pixel
  *
@@ -128,12 +80,12 @@ float lunghezza_focale(int larghezza_immagine, float fov_orizzontale)
 /**
  * Funzione che scrive del testo sull'immagine in un punto specifico
  *
- * @param[in] immagine immagine su cui scrivere.
- * @param[in] punto posizione dove scrivere.
- * @param[in] stringa testo da scrivere.
+ * @param immagine immagine su cui scrivere.
+ * @param punto posizione dove scrivere.
+ * @param stringa testo da scrivere.
  *
  */
-void scrivi_testo(Mat &immagine, Point2i &punto, char stringa[])
+void scrivi_testo(cv::Mat &immagine, cv::Point2i &punto, char stringa[])
 {
     cv::Scalar colore_centro(0, 255, 0);
     cv::Point2i tralsazione_testo(0, -25);
@@ -144,9 +96,11 @@ void scrivi_testo(Mat &immagine, Point2i &punto, char stringa[])
 /**
  * Calcolo del FOV orizzontale a partire dal FOV diagonale e l'aspect ratio
  *
- * @param[in] Df fov diagonale in gradi.
- * @param[in] Ha aspect ratio orizzontale (es: il 16 dei 16:9).
- * @param[in] Va aspect ratio verticale (es: il 9 dei 16:9).
+ * @authors Raffaele Giacomo Giovanni Di Maio, Giulia Signori, Emilio Meroni
+ * 
+ * @param Df fov diagonale in gradi.
+ * @param Ha aspect ratio orizzontale (es: il 16 dei 16:9).
+ * @param Va aspect ratio verticale (es: il 9 dei 16:9).
  *
  * @return fov orizzontale in gradi
  *
@@ -173,10 +127,11 @@ float fov_orizzontale(float Df, float Ha, float Va)
     float Hf_rad = 2 * atan(tan(Df_rad / 2) * (Ha / Da));
     return rad2deg(Hf_rad);
 }
+
 /**
  * conversione da radianti a gradi.
  *
- * @param[in] rad valore in radianti
+ * @param rad valore in radianti
  *
  * @return angolo in gradi
  *
@@ -190,7 +145,7 @@ float rad2deg(float rad)
 /**
  * conversione da gradi a radianti.
  *deg2rad(Df)
- * @param[in] deg valore in gradi
+ * @param deg valore in gradi
  *
  * @return angolo in radianti
  *
@@ -201,4 +156,26 @@ float rad2deg(float rad)
 float deg2rad(float deg)
 {
     return deg * CV_PI / 180.0;
+}
+
+int main()
+{
+    cv::VideoCapture video_capture(0);
+    cv::Mat frame;
+    cv::namedWindow(nome_window_immagine);
+    create_trackbar();
+    char testo[100];
+
+    while (true)
+    {
+        video_capture >> frame;
+        cv::Point2i center_image = get_center_image(frame);
+        cv::Point2f distanza = calcola_distanza_centro(((float)altezza_int) / 100, ((float)angolo_deg_int) / 100);
+        sprintf(testo, "distanza: %.2f m", distanza.x);
+        scrivi_testo(frame, center_image, testo);
+        cv::imshow(nome_window_immagine, frame);
+        cv::waitKey(10);
+    }
+
+    video_capture.release();
 }
